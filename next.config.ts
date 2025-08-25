143742385ea128785e33ecd42f651f72113dc9f4
ニヒット <xnishidh.codes@gmail.com>
2025-08-25 18:40:03 +0530

    web_client_1.1.2

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // for static export
-  images: {
-    unoptimized: true, // required for static export
-  },
-  basePath: "/Nifolio", // your repo name
-  assetPrefix: "/Nifolio/", // ensures static assets load from correct path
+  /* config options here */
+  
 };
 
 export default nextConfig;

dd14d7fd24e4201bd9337ecd01064654addcb0a1
ニヒット <xnishidh.codes@gmail.com>
2025-08-25 18:29:13 +0530

    web_client_1.1.1

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/Nifolio", // repo name
+  assetPrefix: "/Nifolio/", // for static assets like images
 };
 
 export default nextConfig;

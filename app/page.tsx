15d6db7f48f68406d981a1bbf25acf713291b98a
Nishidh Singh <xnishidh.codes@gmail.com>
2025-09-21 16:55:23 +0530

    added smooth scroll

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import IndexPage from "@/components/HomeScreen/IndexPage";
 export default function Home() {
 
   return (
-    <div className="min-h-screen max-w-screen bg-gradient-to-b from-black via-gray-950 to-gray-900 overflow-auto">
+    <div className="min-h-screen max-w-screen bg-gradient-to-b from-black via-gray-950 to-gray-900 overflow-auto scroll-smooth">
       <IndexPage />
 
     </div>
